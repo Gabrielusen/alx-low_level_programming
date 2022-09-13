7ef873cfd2ead1c805ec1f6c8ce06d64b08f6b84
gabrielusen <gabrielufot23@gmail.com>
2022-09-13 10:57:03 +0100

    4-puts.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,6 +6,7 @@
  */
 int main(void)
 {
+	int integerType
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n" sizeof(long int));
