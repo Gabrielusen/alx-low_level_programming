280636071fe6883730ee1ced603389987570b9e3
gabrielusen <gabrielufot23@gmail.com>
2022-09-19 13:14:44 +0100

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,19 +6,21 @@
  *
  * Return: Always 0
  */
+
 void jack_bauer(void)
 {
-	int a;
-	int b;
+	int hour, minute;
 
-	for (a = 0; a <= 23; a++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		_putchar (a / 10 + '0');
-		_putchar (a % 10 + '0');
-		_putchar (':');
-		_putchar (b / 10 + '0');
-		_putchar (b % 10 + '0');
-		_putchar ('\n');
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
-
