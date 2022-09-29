fc0dccb1ccea46d1ab8aed0277a6f50334ddf5f2
gabrielusen <gabrielufot23@gmail.com>
2022-09-29 15:04:29 +0100

    it prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,29 +7,16 @@
  */
 void print_number(int n)
 {
-	unsigned int i. j, count;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		i = n * -1;
+		_putchar('-');
+		num = -num;
 	}
-	else
+	if ((num / 10) > 0)
 	{
-		i = n;
+		print_number(num / 10);
 	}
-
-	j = i;
-	count = 1;
-
-	while (j > 9)
-	{
-		j /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((i / count) % 10) + 48);
-	}
+	_putchar((num % 10) + '0');
 }
