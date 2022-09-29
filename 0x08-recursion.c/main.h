e28a80e3e7a0027a2497868c12897d6162698080
gabrielusen <gabrielufot23@gmail.com>
2022-09-29 16:17:27 +0100

    done

--- 0x08-recursion.c/main.h
+++ 0x08-recursion.c/main.h
@@ -1,8 +1,8 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char);
-void _putd_recursion(char *s);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
