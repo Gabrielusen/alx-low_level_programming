585e471c8602cd1c8ed51aa2924dd2b087f7ebb9
gabrielusen <gabrielufot23@gmail.com>
2022-10-06 14:44:24 +0100

    correct

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,37 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * string_nconcat - function that concatenates two strings.
+ * *string_nconcat - concatenates 2 strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * @n: number of bytes to use from s2
  *
- * @s1: first string
- * @s2: second string
- * @n: memory size
- *
- * Return: a pointer to the string array of the 2 strings.
+ * Return: pointer to newly created string, else NULL if failure occurs
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s3;
-	unsigned int i, len1, len2, total_len;
+	char *str;
+	unsigned int index, index2, len1, len2;
 
-	len1 = 0;
-	len2 = 0;
-	
-	if (s1 == NULL)
+	if (s1 == 0)
 		s1 = "";
-	if (s2 == NULL)
+	if (s2 == 0)
 		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
+	len1 = 0;
+	while (*(s1 + len1))
 		len1++;
-	for (i = 0; s2[i] != '\0'; i++)
+	len2 = 0;
+	while (*(s2 + len2))
 		len2++;
 	if (n >= len2)
 		n = len2;
-	
-	total_len = len1 + n;
-
-	s3 = malloc(sizeof(char) * (total_len + 1));
-	if (s3 == NULL)
-		return (NULL);
-	for (i = 0; i < len1; i++)
-		s3[i] = s1[i];
-	for (i = 0; i < n; i++)
-		s3[len1 + i] = s2[i];
-	s3[len1 + i] = '\0';
-	
-	return (s3);
+	str = malloc(sizeof(char) * (len1 + n + 1));
+	if (str == 0)
+		return (0);
+	for (index = 0; index < len1; index++)
+		*(str + index) = *(s1 + index);
+	for (index2 = 0; index2 < n; index2++, index++)
+		*(str + index) = *(s2 + index2);
+	*(str + index) = '\0';
+	return (str);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,31 +1,26 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * array_range - function that creates an array of integers.
+ * *array_range - creates an array of integers and stores numbers min to max,
+ * both inclusive in the array
+ * @min: smallest number to be stored
+ * @max: largest number to be stored
  *
- * @min: minimum integer
- * @max: maximum integer
- *
- * Return: the pointer to the newly created array
+ * Return: pointer to the newly created array on success,
+ * else NULL if min > max or if malloc fails
  */
-
 int *array_range(int min, int max)
 {
-	int *arr, i, len;
-	
+	int *arr;
+	int len, num, index;
+
 	if (min > max)
-		return (NULL);
+		return (0);
 	len = (max - min) + 1;
-	
 	arr = malloc(sizeof(int) * len);
-	if (arr == NULL)
-		return (NULL);
-	
-	for (i = 0; i < len; i++)
-	{
-		arr[i] = min;
-		min++;
-	}
-	
+	if (arr == 0)
+		return (0);
+	for (num = min, index = 0; num <= max; index++, num++)
+		*(arr + index) = num;
 	return (arr);
 }
